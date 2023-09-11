3d80c2a782fed23593c99db6617f9f2df42c3889
Goran <goranvujnovic85@gmail.com>
2023-09-11 19:19:23 +0200

    third commit

--- Main.c
+++ Main.c
@@ -17,6 +17,7 @@ Description :
 
 void fillMatrix(int matrix[3][4], int rows, int cols, char fillType)
 {
+
 	if(fillType == 'A')
 	{
 		int value = 1;
@@ -33,9 +34,10 @@ void fillMatrix(int matrix[3][4], int rows, int cols, char fillType)
 
 	else if(fillType == 'B')
 	{
-		int value = 12;
 
-		for(int i = 0; i < rows; i++)
+		int value = 12;
+      /*https://www.youtube.com/watch?v=hWhMAoBhi7M&list=RDCMUCnaDKXKHRc2_V6shuoZMpiA&start_radio=1&rv=hWhMAoBhi7M&t=2673*/
+		for(int i = 2; i < rows; i--)
 		{
 			for(int j = 0; j < cols; j++)
 			{
@@ -51,35 +53,31 @@ void fillMatrix(int matrix[3][4], int rows, int cols, char fillType)
 void printMatrix(int matrix[3][4], int rows, int cols)
  {
 	for(int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j < cols; j++)
 		{
-			for(int j = 0; j < cols; j++)
-			{
-				printf("\t[%d][%d]=[%d]\n", i, j, matrix[i][j]);
-				fflush(stdout);
-			}
+			/*char slovo = 'A' + matrix[i][j];
+			printf("\t%c", slovo);
+			*/
+
+			printf("\t%d", matrix[i][j]);
+			fflush(stdout);
 		}
+		printf("\n");
+	}
  }
 
 
-int additionMatrices(int matrix_A[3][4], int matrix_B[3][4], int matrix_C[3][4] , int rows, int cols)
-{
+void additionMatrices(int matrix_A[3][4], int matrix_B[3][4], int matrix_C[3][4], int rows, int cols)
+ {
 	for(int i = 0; i < rows; i++)
 		{
 			for(int j = 0; j < cols; j++)
 		    {
 				matrix_C[i][j] = matrix_A[i][j] + matrix_B[i][j];
 		    }
-
 		}
-			for(int i = 0; i < rows; i++)
-			{
-				for(int j = 0; j < cols; j++)
-				{
-					printf("\t[%d][%d] = [%d]\n", i, j, matrix_C[i][j]);
-			        fflush(stdout);
-			    }
-			}
-}
+ }
 
 int main()
 {
@@ -90,15 +88,22 @@ int main()
 	int matrix_C[3][4];
 
     fillMatrix(matrix_A, rows, cols, 'A');
+
     fillMatrix(matrix_B, rows, cols,  'B');
+
     printf("Matrica A:\n");
     printMatrix(matrix_A, rows, cols);
     printf("\n");
+
     printf("Matrica B:\n");
     printMatrix(matrix_B, rows, cols);
     printf("\n");
-    printf("Matrica C:\n");
+
+
     additionMatrices(matrix_A, matrix_B, matrix_C , rows, cols);
+
+    printf("Matrica C:\n");
+    printMatrix(matrix_C, rows, cols);
 
     return 0;
 }
